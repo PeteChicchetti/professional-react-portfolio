67752c554c654708cd3c0546bf591508164c25c9
PeteChicchetti <pj0524@gmail.com>
2022-12-03 14:14:33 -0500

    add App page logic

--- src/App.js
+++ src/App.js
@@ -8,3 +8,29 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Nav from "./components/Nav";
 import Portfolio from "./components/Portfolio";
+
+// App function
+function App() {
+  const [pageIndex, setPageIndex] = useState(0);
+
+  return (
+    <div className='App'>
+      <Nav 
+        showAbout={() => setPageIndex(0)}
+        showPortfolio={() => setPageIndex(1)}
+        showContact={() => setPageIndex(2)}
+      />
+      {pageIndex === 0 ? (
+        <About />
+      ) : pageIndex === 1 ? (
+        <Portfolio />
+      ) : pageIndex === 2 ? (
+        <Contact />
+      ) : pageIndex === 3 ? (
+        <Resume />
+      ) : (
+        <About />
+      )}
+    </div>
+  );
+}

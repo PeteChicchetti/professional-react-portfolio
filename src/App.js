8eab1ca657e3b62f2c0de87e4bb64c794fbbc5ae
PeteChicchetti <pj0524@gmail.com>
2022-12-03 14:28:14 -0500

    added resume to app import

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Nav from "./components/Nav";
 import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume"
 
 // App function
 function App() {

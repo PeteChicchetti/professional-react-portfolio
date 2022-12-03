356b54a6c6011c02e80275941efc303f8e604e98
PeteChicchetti <pj0524@gmail.com>
2022-12-03 13:55:04 -0500

    added component folders and files

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <header>
+            <div className="container">
+                <h1>Pete Chicchetti</h1>
+                <ul>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Portfolio</li>
+                    <li>Resume</li>
+                </ul>
+            </div>
+        </header>
+    );
+};
+
+export default Nav;

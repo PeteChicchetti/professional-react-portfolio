9e554403f7e40c2afe84108fb36a00ff865b2ffa
PeteChicchetti <pj0524@gmail.com>
2022-12-04 18:04:17 -0500

    fixed margin for mobile

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,7 +4,7 @@ import { Row, Col } from "react-bootstrap";
 const Nav = ({showAbout, showPortfolio, showContact, showResume}) => {
     return (
         <div className='container-fluid' id='headerBg' >
-            <Row  className=''style={{ alignItems: 'center'}}>
+            <Row  className='container-fluid'style={{ alignItems: 'center', margin: '0px'}}>
                 <Col md='6' id='title'>
                     <div className='nameContainer'>
                         <h1 id='name'>Pete Chicchetti</h1>
@@ -12,7 +12,7 @@ const Nav = ({showAbout, showPortfolio, showContact, showResume}) => {
                 </Col>
                 <Col md='6' id=''>
                 <ul className='nav'>
-                    <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showAbout}>About</li>
+                    <li className="nav-list" style={{ marginLeft: "0px" }} onClick={showAbout}>About</li>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showContact}>Contact</li>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showPortfolio}>Portfolio</li>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showResume}>Resume</li>

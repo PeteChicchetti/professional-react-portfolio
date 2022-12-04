4d0cc91e70aae7600b647271c3cb25f555e920a1
PeteChicchetti <pj0524@gmail.com>
2022-12-04 00:07:02 -0500

    adjusted padding on header and nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -5,13 +5,13 @@ const Nav = ({showAbout, showPortfolio, showContact, showResume}) => {
     return (
         <Container className=''>
             <Row  className=''style={{ alignItems: 'center'}}>
-                <Col md='6' className=''>
-                    <div style={{ display: "flex", justifyContent: 'start'}}>
+                <Col md='6' id='title'>
+                    <div style={{ display: "flex", justifyContent: 'center'}}>
                         <h1 id='name'>Pete Chicchetti</h1>
                     </div>
                 </Col>
-                <Col md='6'>
-                <ul id='nav' style={{ display: "flex", listStyle: "none", paddingRight: "20px", justifyContent: 'end' }}>
+                <Col md='6' id='nav'>
+                <ul id='nav' style={{ display: "flex", listStyle: "none", justifyContent: 'center' }}>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showAbout}>About</li>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showContact}>Contact</li>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showPortfolio}>Portfolio</li>

c4d86d5e2d31a1a93bab3ff743b37b04eb605505
PeteChicchetti <pj0524@gmail.com>
2022-12-03 22:17:55 -0500

    added footer and css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
     let date = new Date();
     let year = date.getFullYear();
     return (
-        <div className="containerFluid footer">
-        <div className='' style={{ display: 'flex', justifyContent: 'space-between'}}>
-            <div className="copywright">
+        <Container fluid className=" footer">
+        <Row className=''>
+            <Col md='4' className='copywright'>
                 <h3>Designed and Developed by Pete Chicchetti</h3>
-            </div>
-            <div className="copywright">
-            <h3>Copyright © {year} Pete Chicchetti</h3>
-            </div>
-        </div>
-        </div>
+            </Col>
+            <Col md='4' className='copywright'>
+                <h3>Copyright © {year}</h3>
+            </Col>
+            <Col md='4'>
+                <Col>
+                    <span className='foot-logos'>
+                        <a className='logos' href="https://github.com/PeteChicchetti">GitHub</a>
+                    </span>
+                    <span>
+                        <a className='logos' href="https://www.linkedin.com/in/peter-chicchetti-268a39224/">Linked In</a>
+                    </span>
+                </Col>
+            </Col>
+        </Row>
+        </Container>
     )
 }
 
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Nav = ({showAbout, showPortfolio, showContact, showResume}) => {
     return (
         <header>
-            <div className="containerFluid" style= {{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+            <div className="containerFluid" style= {{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingLeft: "20px"}}>
                 <h1>Pete Chicchetti</h1>
-                <ul style={{ display: "flex", listStyle: "none"}}>
+                <ul style={{ display: "flex", listStyle: "none", paddingRight: "20px" }}>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showAbout}>About</li>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showContact}>Contact</li>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showPortfolio}>Portfolio</li>

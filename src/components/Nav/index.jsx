289ba5fa0ed1c57854cb7fd90b74d41c622d3b35
PeteChicchetti <pj0524@gmail.com>
2022-12-04 13:18:17 -0500

    adjusted content spacing for nav/mobile nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -6,12 +6,12 @@ const Nav = ({showAbout, showPortfolio, showContact, showResume}) => {
         <Container className=''>
             <Row  className=''style={{ alignItems: 'center'}}>
                 <Col md='6' id='title'>
-                    <div style={{ display: "flex", justifyContent: 'center'}}>
+                    <div className='nameContainer'>
                         <h1 id='name'>Pete Chicchetti</h1>
                     </div>
                 </Col>
-                <Col md='6' id='nav'>
-                <ul id='nav' style={{ display: "flex", listStyle: "none", justifyContent: 'center' }}>
+                <Col md='6' id=''>
+                <ul className='nav'>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showAbout}>About</li>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showContact}>Contact</li>
                     <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showPortfolio}>Portfolio</li>

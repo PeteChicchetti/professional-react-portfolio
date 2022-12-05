12af418897424dbe652ad270a4a01b278a0a1951
PeteChicchetti <pj0524@gmail.com>
2022-12-05 18:05:21 -0500

    setup contact details and message columns for contact page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <section className='container-fluid contactPage'>
+            <Row id='contactContainer' style={{ margin: '0px'}}>
+                <Col md='4' id='contactLeft' style={{ margin: '0px'}}>
+                    <div className='leftContent'>
+                        <strong className='contactTitle'>Contact Details</strong>
+                        <p><strong>Email Me:</strong><br/>petechicchetti@gmail.com</p>
+                    </div>
+                </Col>
+                <Col md='4' id='contactright' style={{ margin: '0px'}}>
+                    <div className='rightContent'>
+                        <strong className='contactTitle'>Send Me A Message</strong>
+                    </div>
+                </Col>
+            </Row>
+        </section>
     );
 };
 

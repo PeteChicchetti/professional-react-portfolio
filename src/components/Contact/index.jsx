15dd2ddbbae868391ca7f315286a7e4f7aa10a19
PeteChicchetti <pj0524@gmail.com>
2022-12-05 18:25:16 -0500

    added logos to contact and adjusted colors when hovered

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const Contact = () => {
     return (
@@ -8,7 +10,9 @@ const Contact = () => {
                 <Col md='4' id='contactLeft' style={{ margin: '0px'}}>
                     <div className='leftContent'>
                         <strong className='contactTitle'>Contact Details</strong>
-                        <p><strong>Email Me:</strong><br/>petechicchetti@gmail.com</p>
+                        <p id='info'><strong>Email Me:</strong><br/>petechicchetti@gmail.com</p>
+                        <a className='githubContact' href="https://github.com/PeteChicchetti" target="_blank" rel="noopener noreferrer"><AiFillGithub /></a>
+                        <a className='linkedinContact' href="https://www.linkedin.com/in/peter-chicchetti-268a39224/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
                     </div>
                 </Col>
                 <Col md='4' id='contactright' style={{ margin: '0px'}}>

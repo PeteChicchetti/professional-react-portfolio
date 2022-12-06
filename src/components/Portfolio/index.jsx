08dbcd98d9979d5ad3657d36996e8cc8f5883026
PeteChicchetti <pj0524@gmail.com>
2022-12-06 17:21:41 -0500

    adjusted title and paragraph padding and margins

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -7,33 +7,33 @@ const Portfolio = () => {
             <Row className='container-fluid' id='cardContain'  sm='12'>
                 <Card id='card' lg='4' md='6' sm='2'>
                     <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4>VivoVirtuoso</h4>
-                    <p>This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <h4 className="cardTitle">VivoVirtuoso</h4>
+                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                </Card>
+                <Card id='card'>
+                    <img className="cardImg" src="images/AutoAssistant.png" alt="AutoAssistant homepage"></img>
+                    <h4 className="cardTitle">AutoAssistant</h4>
+                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
                 </Card>
                 <Card id='card'>
                     <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4>VivoVirtuoso</h4>
-                    <p>This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <h4 className="cardTitle">VivoVirtuoso</h4>
+                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
                 </Card>
                 <Card id='card'>
                     <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4>VivoVirtuoso</h4>
-                    <p>This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <h4 className="cardTitle">VivoVirtuoso</h4>
+                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
                 </Card>
                 <Card id='card'>
                     <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4>VivoVirtuoso</h4>
-                    <p>This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <h4 className="cardTitle">VivoVirtuoso</h4>
+                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
                 </Card>
                 <Card id='card'>
                     <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4>VivoVirtuoso</h4>
-                    <p>This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
-                </Card>
-                <Card id='card'>
-                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4>VivoVirtuoso</h4>
-                    <p>This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <h4 className="cardTitle">VivoVirtuoso</h4>
+                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
                 </Card>
             </Row>
         </Container>

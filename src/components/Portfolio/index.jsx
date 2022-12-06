2426bb40dcce7aa6eebd01c00cb66e0bab995927
PeteChicchetti <pj0524@gmail.com>
2022-12-06 16:27:40 -0500

    added card image placeholders and border styling

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,37 +1,37 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Card } from "react-bootstrap";
 
 const Portfolio = () => {
     return (
         <Container id='portContainer' style={{ margin: 'auto' }}>
             <Row id='cardContain'  sm='12'>
                 <Card id='card' lg='4' md='6' sm='12'>
-                    <img></img>
+                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
+                    <h4>VivoVirtuoso</h4>
+                    <p>This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                </Card>
+                <Card id='card'>
+                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>
                 <Card id='card'>
-                    <img></img>
+                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>
                 <Card id='card'>
-                    <img></img>
+                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>
                 <Card id='card'>
-                    <img></img>
+                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>
                 <Card id='card'>
-                    <img></img>
-                    <h4>Project Name</h4>
-                    <p>Description</p>
-                </Card>
-                <Card id='card'>
-                    <img></img>
+                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>

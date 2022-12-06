9e2e283362788855deec6b3a94a08018007fc5b4
PeteChicchetti <pj0524@gmail.com>
2022-12-06 16:16:25 -0500

    added flex to boxes, 3/2/1 at screen sizes. drop shadow and container padding

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -3,36 +3,34 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 
 const Portfolio = () => {
     return (
-        <Container>
-            <Row>
-                <Card>
+        <Container id='portContainer' style={{ margin: 'auto' }}>
+            <Row id='cardContain'  sm='12'>
+                <Card id='card' lg='4' md='6' sm='12'>
                     <img></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>
-                <Card>
+                <Card id='card'>
                     <img></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>
-                <Card>
+                <Card id='card'>
                     <img></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>
-            </Row>
-            <Row>
-                <Card>
+                <Card id='card'>
                     <img></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>
-                <Card>
+                <Card id='card'>
                     <img></img>
                     <h4>Project Name</h4>
                     <p>Description</p>
                 </Card>
-                <Card>
+                <Card id='card'>
                     <img></img>
                     <h4>Project Name</h4>
                     <p>Description</p>

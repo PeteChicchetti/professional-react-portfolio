1404ad42da42655b66fac96167ac4f77eef416f7
PeteChicchetti <pj0524@gmail.com>
2022-12-05 21:07:08 -0500

    added animated css to nav links

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,18 +4,18 @@ import { Row, Col } from "react-bootstrap";
 const Nav = ({showAbout, showPortfolio, showContact, showResume}) => {
     return (
         <div className='container-fluid' id='headerBg' >
-            <Row  className='container-fluid'style={{ alignItems: 'center', margin: '0px'}}>
+            <Row  className='container-fluid'style={{ alignItems: 'center', margin: '0px', padding: '0px'}}>
                 <Col md='6' id='title'>
                     <div className='nameContainer'>
                         <h1 id='title'>Pete Chicchetti</h1>
                     </div>
                 </Col>
-                <Col md='6' id=''>
+                <Col md='6' id='navlist'>
                 <ul className='nav'>
-                    <li className="nav-list" style={{ marginLeft: "0px" }} onClick={showAbout}>About</li>
-                    <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showContact}>Contact</li>
-                    <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showPortfolio}>Portfolio</li>
-                    <li className="nav-list" style={{ marginLeft: "10px" }} onClick={showResume}>Resume</li>
+                    <li className="nav-link nav-link-ltr" id='navNames' style={{ marginLeft: "0px", padding: '8px 0px'}} onClick={showAbout}>About</li>
+                    <li className="nav-link nav-link-ltr" id='navNames' style={{ marginLeft: "8px", padding: '8px 0px' }} onClick={showContact}>Contact</li>
+                    <li className="nav-link nav-link-ltr" id='navNames' style={{ marginLeft: "8px", padding: '8px 0px' }} onClick={showPortfolio}>Portfolio</li>
+                    <li className="nav-link nav-link-ltr" id='navNames' style={{ marginLeft: "8px", padding: '8px 0px' }} onClick={showResume}>Resume</li>
                 </ul>
                 </Col>
             </Row>

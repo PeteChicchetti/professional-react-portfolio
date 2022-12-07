b8b3a2d24a0edd46a49db6876c36dcb403929f02
PeteChicchetti <pj0524@gmail.com>
2022-12-07 17:45:20 -0500

    added span and moved button to center of doc with styling

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Row, Card } from "react-bootstrap";
+import { BsDownload } from "react-icons/bs";
 
 const Resume = () => {
     return (
         <div id='resumeContainer'>
             <Row className='container-fluid'id='resumeCardContainer'>
-                <Card id='resumeCard'>
-                    <button id='downloadBtn' >Download Resume</button>
+                <Card id='resumeCard'style={{ textAlign:'center'}}>
+                    <span><button id='downloadBtn' ><BsDownload /> Download</button></span>
                     <img id='resume' src='images/resume.png' alt='Resume of Pete Chicchetti' ></img>
                 </Card>
             </Row>

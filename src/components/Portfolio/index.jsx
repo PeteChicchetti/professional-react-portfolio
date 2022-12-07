3f3704a03ec57440ab1110cf8f805fc4ab16bdc0
PeteChicchetti <pj0524@gmail.com>
2022-12-06 19:22:03 -0500

    added text editor, changed descriptions, changed coming soon title and images

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -10,7 +10,7 @@ const Portfolio = () => {
                 <Card id='card' lg='4' md='6' sm='2'>
                     <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
                     <h4 className="cardTitle">VivoVirtuoso</h4>
-                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <p className="cardPara">This is a single page front end application using one input to simultaneosly search two APIs for an artists info and a similar artist.</p>
                     <div>
                         <button id="ghBtn"><AiFillGithub /> GitHub</button>
                         <button id="webBtn"><BsFileEarmarkText /> Preview</button>
@@ -19,43 +19,44 @@ const Portfolio = () => {
                 <Card id='card'>
                     <img className="cardImg" src="images/AutoAssistant.png" alt="AutoAssistant homepage"></img>
                     <h4 className="cardTitle">AutoAssistant</h4>
-                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <p className="cardPara">AutoAssistant uses express, mysql and handlebars. The backend allows for a user to login and search for vehicles and save them to the user by session.</p>
                     <div>
                         <button id="ghBtn"><AiFillGithub /> GitHub</button>
                         <button id="webBtn"><BsFileEarmarkText /> Preview</button>
                     </div>
                 </Card>
                 <Card id='card'>
-                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4 className="cardTitle">VivoVirtuoso</h4>
-                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <img className="cardImg" src="images/pwa-text-editor.png" alt="VivoVirtuoso homepage"></img>
+                    <h4 className="cardTitle">J.A.T.E.</h4>
+                    <p className="cardPara">Just Another Text Editor<br/>
+                    This PWA is installable and allows for the user to save notes to an indexedDb for offline use. </p>
                     <div>
                         <button id="ghBtn"><AiFillGithub /> GitHub</button>
                         <button id="webBtn"><BsFileEarmarkText /> Preview</button>
                     </div>
                 </Card>
                 <Card id='card'>
-                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4 className="cardTitle">VivoVirtuoso</h4>
-                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <img className="cardImg" src="images/coming-soon.jpg" alt="VivoVirtuoso homepage"></img>
+                    <h4 className="cardTitle">Coming Soon</h4>
+                    <p className="cardPara"></p>
                     <div>
                         <button id="ghBtn"><AiFillGithub /> GitHub</button>
                         <button id="webBtn"><BsFileEarmarkText /> Preview</button>
                     </div>
                 </Card>
                 <Card id='card'>
-                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4 className="cardTitle">VivoVirtuoso</h4>
-                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <img className="cardImg" src="images/coming-soon.jpg" alt="VivoVirtuoso homepage"></img>
+                    <h4 className="cardTitle">Coming Soon</h4>
+                    <p className="cardPara"></p>
                     <div>
                         <button id="ghBtn"><AiFillGithub /> GitHub</button>
                         <button id="webBtn"><BsFileEarmarkText /> Preview</button>
                     </div>
                 </Card>
                 <Card id='card'>
-                    <img className="cardImg" src="images/VivoVirtuoso.png" alt="VivoVirtuoso homepage"></img>
-                    <h4 className="cardTitle">VivoVirtuoso</h4>
-                    <p className="cardPara">This is a single page front end application using two APIs to display an artist and their info as well as a similar artist.</p>
+                    <img className="cardImg" src="images/coming-soon.jpg" alt="VivoVirtuoso homepage"></img>
+                    <h4 className="cardTitle">Coming Soon</h4>
+                    <p className="cardPara"></p>
                     <div>
                         <button id="ghBtn"><AiFillGithub /> GitHub</button>
                         <button id="webBtn"><BsFileEarmarkText /> Preview</button>

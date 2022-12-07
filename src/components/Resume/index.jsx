0c4544d4f40900cf3d53defcb28291ff1a2a5a10
PeteChicchetti <pj0524@gmail.com>
2022-12-07 17:13:49 -0500

    rough layout adding resume image card and container with background

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Row, Card } from "react-bootstrap";
 
 const Resume = () => {
     return (
-        <div>Resume</div>
+        <div id='resumeContainer'>
+            <Row className='container-fluid'id='resumeCardContainer'>
+                <Card id='resumeCard'>
+                    <button id='downloadBtn' >Download Resume</button>
+                    <img id='resume' src='images/resume.png' alt='Resume of Pete Chicchetti' ></img>
+                </Card>
+            </Row>
+        </div>
     );
 };
 

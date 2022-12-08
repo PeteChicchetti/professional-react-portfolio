4a56248ff4e7cd2c0f35bd2f70d2be1a4f13d12b
PeteChicchetti <pj0524@gmail.com>
2022-12-07 19:07:45 -0500

    changed about me section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -7,8 +7,7 @@ const About = () => {
         <div id='bg' >
             <Row className='container-fluid' id='aboutContent' style={{textAlign: 'center', margin: '0px' }}>
                 <Col md='8' id='aboutMe'>
-                What is Lorem Ipsum?
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                I'm an aspiring web developer and a recent graduate of the UCF Full Stack Bootcamp. As someone who is detail oriented, passionate and enthusiastic, I take pride in every project. I enjoy learning new technologies and expanding my abilities with each challenge that I overcome. 
                 </Col>
                 <Col md='4' style={{ padding: '1rem'}}>
                 <Col id='logo'>
